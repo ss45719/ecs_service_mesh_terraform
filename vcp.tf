879eaed230258351b197e9feab5f2220ae32c0d0
Shashi Kumar <ss45719@gmail.com>
2022-02-11 01:36:20 +0530

    vpc code

--- vcp.tf
+++ vcp.tf
@@ -1,8 +1,54 @@
 # Create a VPC
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
+  tags = {
+    "Name" = "${var.default_tags.project}-vpc"
+  }
+  assign_generated_ipv6_cidr_block = true
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+  enable_dns_support = true
 }
 
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  # 10.255.0.0/20 --> 10.255.0.0/24
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 4, 0)
+  ipv6_cidr_block = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, 0)
+  map_public_ip_on_launch = true
+  assign_ipv6_address_on_creation = true
+  tags = {
+      "Name" ="${var.default_tags.project}-public-${data.aws_availability_zones.available.names[0]}"
+  }
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main
+  tags = {
+    "Name" = "${var.default_tags.project}-public-route-table"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main
+  tags = {
+    "Name" = "${var.default_tags.project}-igw"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# public subnet $ private subnet
 # public route table and routes
 # private route table and routes
 # public and private subnets
